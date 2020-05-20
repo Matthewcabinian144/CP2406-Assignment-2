9b7587b8140b7c38252410a47c62b8a7f05e0b57
Matthew Cabinian <31027708+Matthewcabinian144@users.noreply.github.com>
2020-05-20 19:34:43 +0800

    Update Main and wrote Window Setup

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import View.EditorPanel;
+import View.SimulationPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
-    private static final int WINDOW_WIDTH = ;
-    private static final int WINDOW_HEIGHT = ;
+    private static final int WINDOW_WIDTH = 1600;
+    private static final int WINDOW_HEIGHT = 1024;
+    private static SimulationPanel simulationPanel = new SimulationPanel();
+    private static EditorPanel editorPanel = new EditorPanel();
+    private static final int SCALE = 8;
 
-
+    public static void main(String[] args) {
+        JFrame mainWindow = new JFrame("Traffic Simulator");
+        mainWindow.setLayout(new BorderLayout());
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindow.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    }
 
 
 
