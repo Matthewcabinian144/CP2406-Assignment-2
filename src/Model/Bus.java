cee25e5721d53e47fa35d9fcf0fcbaa8a9d269b2
Matthew Cabinian <31027708+Matthewcabinian144@users.noreply.github.com>
2020-05-20 22:35:07 +0800

    Update Bus.java

--- src/Model/Bus.java
+++ src/Model/Bus.java
@@ -1,4 +1,12 @@
 package Model;
 
-public class Bus {
+public class Bus extends Vehicle {
+
+    public Bus(String id, Road currentRoad) {
+        super(currentRoad);
+        this.id = ("bus_" + id);
+        setLength(super.getLength() * 3);
+        position = -length;
+    }
+
 }
