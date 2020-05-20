1c775b71056a393dfeb99b17909133da338278a0
Matthew Cabinian <31027708+Matthewcabinian144@users.noreply.github.com>
2020-05-20 22:30:30 +0800

    Update Vehicle.java

--- src/Model/Vehicle.java
+++ src/Model/Vehicle.java
@@ -1,4 +1,20 @@
 package Model;
 
-public class Vehicle {
+import java.awt.*;
+import java.util.Random;
+
+public abstract class Vehicle {
+
+    private static final int STOPPED = 0;
+    private static final int START_POSITION = 0;
+    int length;
+    int breadth;
+    String id;
+    private int speed;
+    private Road currentRoad;
+    int position;
+    private Color colour;
+    private Random random = new Random();
+
+    
 }
