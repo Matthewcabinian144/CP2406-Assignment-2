2bd0569c68a345519e80c9176093a45c2d2c535a
Matthew Cabinian <31027708+Matthewcabinian144@users.noreply.github.com>
2020-05-20 19:38:40 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,10 +13,24 @@ public class Main {
     private static final int SCALE = 8;
 
     public static void main(String[] args) {
+        //Window Setup//
         JFrame mainWindow = new JFrame("Traffic Simulator");
         mainWindow.setLayout(new BorderLayout());
         mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainWindow.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        //Simulation Status Bar//
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new GridLayout(1, 0));
+        bottomPanel.setBorder(BorderFactory.createLoweredSoftBevelBorder());
+        JLabel modeLabel = new JLabel("Mode: ");
+        bottomPanel.add(modeLabel);
+        JLabel statusLabel = new JLabel("Status: ");
+        bottomPanel.add(statusLabel);
+        mainWindow.add(bottomPanel, BorderLayout.SOUTH);
+
+
+
     }
 
 
