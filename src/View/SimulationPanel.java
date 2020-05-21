93cbd7bc8cc45ce627a7f581f598d22ea117d831
Matthew Cabinian <31027708+Matthewcabinian144@users.noreply.github.com>
2020-05-21 09:56:40 +0800

    Update SimulationPanel.java

--- src/View/SimulationPanel.java
+++ src/View/SimulationPanel.java
@@ -1,4 +1,32 @@
 package View;
 
-public class SimulationPanel {
+import Model.Road;
+import Model.TrafficLight;
+import Model.Vehicle;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class SimulationPanel extends JPanel {
+
+    public enum State {
+        STOPPED, RUNNING, FINISHED
+    }
+
+    private State state = State.STOPPED;
+    private int scale;
+    private ArrayList<Road> roads;
+    private ArrayList<Vehicle> vehicles = new ArrayList<>();
+    private ArrayList<TrafficLight> lights;
+    private Timer timer;
+    private Boolean stop = true;
+    private Random random = new Random();
+    private static int cycle = 0;
+    private int vehiclesToSpawn = 2;
+    private int vehiclesSpawned = 0;
+    private int vehiclesRemoved = 0;
+    private int numberOfCycles = 20;
+    private int updateRate = 1000;
+    
 }
