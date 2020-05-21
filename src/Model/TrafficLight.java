13f869cddcb4600635c3dae46b30b0e9a5cd2115
Matthew Cabinian <31027708+Matthewcabinian144@users.noreply.github.com>
2020-05-21 10:21:08 +0800

    Update TrafficLight.java

--- src/Model/TrafficLight.java
+++ src/Model/TrafficLight.java
@@ -1,4 +1,14 @@
 package Model;
 
 public class TrafficLight {
+    private static final double CHANGE = 0.4; // more often red
+    private static final String GREEN = "green";
+    private static final String RED = "red";
+    private String id;
+    private String state;
+    private int position;
+    private Road roadAttachedTo;
+
+
+
 }
