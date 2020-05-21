1b25b5d43d5f635c9afda332d749be18fcf12cf7
Matthew Cabinian <31027708+Matthewcabinian144@users.noreply.github.com>
2020-05-21 09:55:30 +0800

    Update EditorPanel.java

--- src/View/EditorPanel.java
+++ src/View/EditorPanel.java
@@ -1,9 +1,15 @@
 package View;
 
+import Model.Road;
+import Model.TrafficLight;
+
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class EditorPanel extends JPanel {
-
+    private ArrayList<Road> roads;
+    private ArrayList<TrafficLight> lights;
+    private int scale;
 
 
 }
