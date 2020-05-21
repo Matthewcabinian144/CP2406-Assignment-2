1115f93984729cb2f664082a181654f687f4da70
Matthew Cabinian <31027708+Matthewcabinian144@users.noreply.github.com>
2020-05-21 20:27:26 +0800

    Update CarTest.java

--- src/Test/CarTest.java
+++ src/Test/CarTest.java
@@ -1,4 +1,9 @@
 package Test;
 
+import Model.Road;
+
 public class CarTest {
+    Model.Road road = new Model.Road("0", 1, 5, new int[]{0, 0}, Road.Orientation.VERTICAL);
+    Model.Car car = new Model.Car("0", road);
+
 }
