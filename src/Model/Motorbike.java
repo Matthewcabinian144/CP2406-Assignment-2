609f423179b8c9d5baae4a9358484b1defd5494f
Matthew Cabinian <31027708+Matthewcabinian144@users.noreply.github.com>
2020-05-21 10:10:10 +0800

    Update Motorbike.java

--- src/Model/Motorbike.java
+++ src/Model/Motorbike.java
@@ -1,4 +1,13 @@
 package Model;
 
-public class Motorbike {
+public class Motorbike extends Vehicle {
+
+    public Motorbike(String id, Road currentRoad) {
+        super(currentRoad);
+        this.id = ("bike_" + id);
+        setLength(super.getLength() / 2);
+        breadth = super.getBreadth() / 2;
+        position = -length;
+    }
+
 }
